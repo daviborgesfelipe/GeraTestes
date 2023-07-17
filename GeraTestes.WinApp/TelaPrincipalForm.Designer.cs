fb107f8a965f7662219d49c30fc7e5df6469828b
Davi Borges <davifelipe_300@hotmail.com>
2023-07-17 15:19:27 -0300

    inicia tela principal

--- GeraTestes.WinApp/TelaPrincipalForm.Designer.cs
+++ GeraTestes.WinApp/TelaPrincipalForm.Designer.cs
@@ -23,17 +23,264 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            menu = new MenuStrip();
+            cadastrosToolStripMenuItem = new ToolStripMenuItem();
+            disciplinaMenuItem = new ToolStripMenuItem();
+            materiasMenuItem = new ToolStripMenuItem();
+            questoesMenuItem = new ToolStripMenuItem();
+            testesMenuItem = new ToolStripMenuItem();
+            btnInserir = new ToolStripButton();
+            btnEditar = new ToolStripButton();
+            btnDuplicar = new ToolStripButton();
+            btnExcluir = new ToolStripButton();
+            btnVisualizar = new ToolStripButton();
+            btnFiltrar = new ToolStripButton();
+            btnGerarPdf = new ToolStripButton();
+            toolStripSeparator2 = new ToolStripSeparator();
+            toolStripSeparator3 = new ToolStripSeparator();
+            toolStripSeparator1 = new ToolStripSeparator();
+            toolStripSeparator4 = new ToolStripSeparator();
+            labelRodape = new ToolStripStatusLabel();
+            toolbox = new ToolStrip();
+            labelTipoCadastro = new ToolStripLabel();
+            panelRegistros = new Panel();
+            statusStrip1 = new StatusStrip();
+            menu.SuspendLayout();
+            toolbox.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menu
+            // 
+            menu.ImageScalingSize = new Size(20, 20);
+            menu.Items.AddRange(new ToolStripItem[] { cadastrosToolStripMenuItem });
+            menu.Location = new Point(0, 0);
+            menu.Name = "menu";
+            menu.Size = new Size(686, 24);
+            menu.TabIndex = 0;
+            menu.Text = "menuStrip1";
+            // 
+            // cadastrosToolStripMenuItem
+            // 
+            cadastrosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { disciplinaMenuItem, materiasMenuItem, questoesMenuItem, testesMenuItem });
+            cadastrosToolStripMenuItem.Name = "cadastrosToolStripMenuItem";
+            cadastrosToolStripMenuItem.Size = new Size(71, 20);
+            cadastrosToolStripMenuItem.Text = "Cadastros";
+            // 
+            // disciplinaMenuItem
+            // 
+            disciplinaMenuItem.Name = "disciplinaMenuItem";
+            disciplinaMenuItem.ShortcutKeys = Keys.F1;
+            disciplinaMenuItem.Size = new Size(149, 22);
+            disciplinaMenuItem.Text = "Disciplinas";
+            // 
+            // materiasMenuItem
+            // 
+            materiasMenuItem.Name = "materiasMenuItem";
+            materiasMenuItem.ShortcutKeys = Keys.F2;
+            materiasMenuItem.Size = new Size(149, 22);
+            materiasMenuItem.Text = "Matérias";
+            // 
+            // questoesMenuItem
+            // 
+            questoesMenuItem.Name = "questoesMenuItem";
+            questoesMenuItem.ShortcutKeys = Keys.F3;
+            questoesMenuItem.Size = new Size(149, 22);
+            questoesMenuItem.Text = "Questões";
+            // 
+            // testesMenuItem
+            // 
+            testesMenuItem.Name = "testesMenuItem";
+            testesMenuItem.ShortcutKeys = Keys.F4;
+            testesMenuItem.Size = new Size(149, 22);
+            testesMenuItem.Text = "Testes";
+            // 
+            // btnInserir
+            // 
+            btnInserir.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnInserir.Image = Properties.Resources.add_circle_FILL0_wght400_GRAD0_opsz24;
+            btnInserir.ImageScaling = ToolStripItemImageScaling.None;
+            btnInserir.ImageTransparentColor = Color.Magenta;
+            btnInserir.Name = "btnInserir";
+            btnInserir.Padding = new Padding(5);
+            btnInserir.Size = new Size(38, 38);
+            // 
+            // btnEditar
+            // 
+            btnEditar.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnEditar.Image = Properties.Resources.edit_FILL0_wght400_GRAD0_opsz24;
+            btnEditar.ImageScaling = ToolStripItemImageScaling.None;
+            btnEditar.ImageTransparentColor = Color.Magenta;
+            btnEditar.Name = "btnEditar";
+            btnEditar.Padding = new Padding(5);
+            btnEditar.Size = new Size(38, 38);
+            // 
+            // btnDuplicar
+            // 
+            btnDuplicar.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnDuplicar.Image = Properties.Resources.control_point_duplicate_FILL0_wght400_GRAD0_opsz24;
+            btnDuplicar.ImageScaling = ToolStripItemImageScaling.None;
+            btnDuplicar.ImageTransparentColor = Color.Magenta;
+            btnDuplicar.Name = "btnDuplicar";
+            btnDuplicar.Padding = new Padding(5);
+            btnDuplicar.Size = new Size(38, 38);
+            // 
+            // btnExcluir
+            // 
+            btnExcluir.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnExcluir.Image = Properties.Resources.delete_FILL0_wght400_GRAD0_opsz24;
+            btnExcluir.ImageScaling = ToolStripItemImageScaling.None;
+            btnExcluir.ImageTransparentColor = Color.Magenta;
+            btnExcluir.Name = "btnExcluir";
+            btnExcluir.Padding = new Padding(5);
+            btnExcluir.Size = new Size(38, 38);
+            // 
+            // btnVisualizar
+            // 
+            btnVisualizar.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnVisualizar.Image = Properties.Resources.search_FILL0_wght400_GRAD0_opsz24;
+            btnVisualizar.ImageScaling = ToolStripItemImageScaling.None;
+            btnVisualizar.ImageTransparentColor = Color.Magenta;
+            btnVisualizar.Name = "btnVisualizar";
+            btnVisualizar.Padding = new Padding(5);
+            btnVisualizar.Size = new Size(38, 38);
+            // 
+            // btnFiltrar
+            // 
+            btnFiltrar.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnFiltrar.Image = Properties.Resources.tune_FILL0_wght400_GRAD0_opsz24;
+            btnFiltrar.ImageScaling = ToolStripItemImageScaling.None;
+            btnFiltrar.ImageTransparentColor = Color.Magenta;
+            btnFiltrar.Name = "btnFiltrar";
+            btnFiltrar.Padding = new Padding(5);
+            btnFiltrar.Size = new Size(38, 38);
+            // 
+            // btnGerarPdf
+            // 
+            btnGerarPdf.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            btnGerarPdf.Image = Properties.Resources.picture_as_pdf_FILL0_wght400_GRAD0_opsz24;
+            btnGerarPdf.ImageScaling = ToolStripItemImageScaling.None;
+            btnGerarPdf.ImageTransparentColor = Color.Magenta;
+            btnGerarPdf.Name = "btnGerarPdf";
+            btnGerarPdf.Padding = new Padding(5);
+            btnGerarPdf.Size = new Size(38, 38);
+            // 
+            // toolStripSeparator2
+            // 
+            toolStripSeparator2.Name = "toolStripSeparator2";
+            toolStripSeparator2.Size = new Size(6, 41);
+            // 
+            // toolStripSeparator3
+            // 
+            toolStripSeparator3.Name = "toolStripSeparator3";
+            toolStripSeparator3.Size = new Size(6, 41);
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(6, 41);
+            // 
+            // toolStripSeparator4
+            // 
+            toolStripSeparator4.Name = "toolStripSeparator4";
+            toolStripSeparator4.Size = new Size(6, 41);
+            // 
+            // labelRodape
+            // 
+            labelRodape.Name = "labelRodape";
+            labelRodape.Size = new Size(52, 17);
+            labelRodape.Text = "[rodapé]";
+            // 
+            // toolbox
+            // 
+            toolbox.Enabled = false;
+            toolbox.ImageScalingSize = new Size(20, 20);
+            toolbox.Items.AddRange(new ToolStripItem[] { btnInserir, btnEditar, btnDuplicar, btnExcluir, toolStripSeparator2, btnVisualizar, toolStripSeparator3, btnGerarPdf, toolStripSeparator1, btnFiltrar, toolStripSeparator4, labelTipoCadastro });
+            toolbox.Location = new Point(0, 24);
+            toolbox.Name = "toolbox";
+            toolbox.Size = new Size(686, 41);
+            toolbox.TabIndex = 1;
+            toolbox.Text = "toolStrip1";
+            // 
+            // labelTipoCadastro
+            // 
+            labelTipoCadastro.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            labelTipoCadastro.Name = "labelTipoCadastro";
+            labelTipoCadastro.Size = new Size(90, 38);
+            labelTipoCadastro.Text = "[tipoCadastro]";
+            // 
+            // panelRegistros
+            // 
+            panelRegistros.Dock = DockStyle.Fill;
+            panelRegistros.Location = new Point(0, 65);
+            panelRegistros.Name = "panelRegistros";
+            panelRegistros.Size = new Size(686, 334);
+            panelRegistros.TabIndex = 3;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.ImageScalingSize = new Size(20, 20);
+            statusStrip1.Items.AddRange(new ToolStripItem[] { labelRodape });
+            statusStrip1.Location = new Point(0, 399);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(686, 22);
+            statusStrip1.TabIndex = 2;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // TelaPrincipalForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(686, 421);
+            Controls.Add(panelRegistros);
+            Controls.Add(statusStrip1);
+            Controls.Add(toolbox);
+            Controls.Add(menu);
+            MainMenuStrip = menu;
+            MinimumSize = new Size(702, 460);
+            Name = "TelaPrincipalForm";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Gerador de Testes 1.0";
+            WindowState = FormWindowState.Maximized;
+            menu.ResumeLayout(false);
+            menu.PerformLayout();
+            toolbox.ResumeLayout(false);
+            toolbox.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelRegistros;
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStrip toolbox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripButton btnInserir;
+        private System.Windows.Forms.ToolStripButton btnEditar;
+        private System.Windows.Forms.ToolStripButton btnExcluir;
+        private System.Windows.Forms.ToolStripButton btnFiltrar;
+        private System.Windows.Forms.ToolStripButton btnGerarPdf;
+        private System.Windows.Forms.ToolStripButton btnDuplicar;
+        private System.Windows.Forms.ToolStripButton btnVisualizar;
+        private System.Windows.Forms.ToolStripLabel labelTipoCadastro;
+        private System.Windows.Forms.ToolStripMenuItem cadastrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disciplinaMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel labelRodape;
+        private System.Windows.Forms.ToolStripMenuItem materiasMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem questoesMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem testesMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
     }
 }
