aff36f2365451c8eab6ee58d7a95646ef0fff073
Davi Borges <davifelipe_300@hotmail.com>
2023-07-18 19:55:43 -0300

    inicia o modulo teste

---
+++ GeraTestes.Dominio/ModuloTeste/Teste.cs
@@ -0,0 +1,54 @@
+﻿using GeraTestes.Dominio.ModuloDisciplina;
+using GeraTestes.Dominio.ModuloMateria;
+using GeraTestes.Dominio.ModuloQuestao;
+
+namespace GeraTestes.Dominio.ModuloTeste
+{
+    public class Teste : EntidadeBase<Teste>
+    {
+        public string Titulo { get; set; }
+        public bool Provao { get; set; }
+        public int QuantidadeQuestoes { get; set; }
+        public bool QuestoesSorteadas { get; set; }
+
+        public List<Questao> Questoes { get; set; }
+        public DateTime DataGeracao { get; set; }
+        public Disciplina Disciplina { get; set; }
+        public Materia Materia { get; set; }
+
+        public override void Atualizar(Teste teste)
+        {
+            Titulo = teste.Titulo;
+            Provao = teste.Provao;
+            DataGeracao = teste.DataGeracao;
+            Disciplina = teste.Disciplina;
+            Materia = teste.Materia;
+            QuantidadeQuestoes = teste.QuantidadeQuestoes;
+            Questoes = teste.Questoes;
+        }
+        public override bool Equals(object obj)
+        {
+            return obj is Teste teste &&
+                   Titulo == teste.Titulo &&
+                   Provao == teste.Provao &&
+                   QuantidadeQuestoes ==  teste.QuantidadeQuestoes &&
+                   QuestoesSorteadas == teste.QuestoesSorteadas;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(
+                Titulo,
+                Provao,
+                QuantidadeQuestoes,
+                QuestoesSorteadas,
+                Questoes,
+                DataGeracao,
+                Disciplina,
+                Materia);
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}", Titulo);
+        }
+    }
+}
