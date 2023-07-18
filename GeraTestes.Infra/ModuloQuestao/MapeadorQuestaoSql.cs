0248bb0816c6455cc4c5727ecf813f6f1a6350e1
Davi Borges <davifelipe_300@hotmail.com>
2023-07-18 17:15:18 -0300

    adiciona mapeador questao

---
+++ GeraTestes.Infra/ModuloQuestao/MapeadorQuestaoSql.cs
@@ -0,0 +1,33 @@
+﻿using GeraTestes.Dominio.ModuloDisciplina;
+using GeraTestes.Dominio.ModuloMateria;
+using GeraTestes.Dominio.ModuloQuestao;
+using GeraTestes.Infra.Sql.Compartilhado;
+using GeraTestes.Infra.Sql.ModuloDisciplina;
+using GeraTestes.Infra.Sql.ModuloMateria;
+
+namespace GeraTestes.Infra.Sql.ModuloQuestao
+{
+    public class MapeadorQuestaoSql : MapeadorBase<Questao>
+    {
+        public override void ConfigurarParametros(SqlCommand comando, Questao questao)
+        {
+            comando.Parameters.AddWithValue("ID", questao.Id);
+            comando.Parameters.AddWithValue("ENUNCIADO", questao.Enunciado);
+            comando.Parameters.AddWithValue("MATERIA_ID", questao.Materia.Id);
+        }
+
+        public override Questao ConverterRegistro(SqlDataReader leitorQuestao)
+        {
+            Disciplina disciplina = new MapeadorDisciplinaSql().ConverterRegistro(leitorQuestao);
+            Materia materia = new MapeadorMateriaSql().ConverterRegistro(leitorQuestao);
+            if (materia != null && disciplina != null)
+                materia.Disciplina = disciplina;
+
+            int id = Convert.ToInt32(leitorQuestao["QUESTAO_ID"]);
+            string enunciado = Convert.ToString(leitorQuestao["QUESTAO_ENUNCIADO"]);
+            Questao questao = new Questao(id, enunciado, materia);
+
+            return questao;
+        }
+    }
+}
