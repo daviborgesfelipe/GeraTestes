970731f9cea32e95c60d59faf0086cdaafafddd7
Davi Borges <davifelipe_300@hotmail.com>
2023-07-18 17:10:32 -0300

    modifica repositorio base adiciona metodo selecionar todos

--- GeraTestes.Infra/Compartilhado/RepositorioEmSqlBase.cs
+++ GeraTestes.Infra/Compartilhado/RepositorioEmSqlBase.cs
@@ -1,10 +1,45 @@
-﻿namespace GeraTestes.Infra.Sql.Compartilhado
+﻿using GeraTestes.Dominio;
+
+namespace GeraTestes.Infra.Sql.Compartilhado
 {
-    public abstract class RepositorioEmSqlBase
+    public abstract class RepositorioEmSqlBase<TEntidade, TMapeador>
+        where TEntidade : EntidadeBase<TEntidade>
+        where TMapeador : MapeadorBase<TEntidade>, new()
     {
         protected static string enderecoBanco =
             @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=GeradorTesteDb;Integrated Security=True";
 
         protected abstract string sqlSelecionarTodos { get; }
+        public virtual List<TEntidade> SelecionarTodos()
+        {
+            //obter a conexão com o banco e abrir ela
+            SqlConnection conexaoComBanco = new SqlConnection(enderecoBanco);
+            conexaoComBanco.Open();
+
+            //cria um comando e relaciona com a conexão aberta
+            SqlCommand comandoSelecionarTodos = conexaoComBanco.CreateCommand();
+            comandoSelecionarTodos.CommandText = sqlSelecionarTodos;
+
+            //executa o comando
+            SqlDataReader leitorItens = comandoSelecionarTodos.ExecuteReader();
+
+            List<TEntidade> registros = new List<TEntidade>();
+
+            TMapeador mapeador = new TMapeador();
+
+            while (leitorItens.Read())
+            {
+                TEntidade registro = mapeador.ConverterRegistro(leitorItens);
+
+                if (registro != null)
+                    registros.Add(registro);
+            }
+
+            //encerra a conexão
+            conexaoComBanco.Close();
+
+            return registros;
+        }
+
     }
 }
