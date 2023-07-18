d9833e5810e457a2578bd17ae773969bad120905
Davi Borges <davifelipe_300@hotmail.com>
2023-07-18 19:24:30 -0300

    inicia user control da tabela questao

---
+++ GeraTestes.WinApp/ModuloQuestao/TabelaQuestaoControl.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace GeraTestes.WinApp.ModuloQuestao
+{
+    partial class TabelaQuestaoControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabelaQuestao = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)tabelaQuestao).BeginInit();
+            SuspendLayout();
+            // 
+            // tabelaQuestao
+            // 
+            tabelaQuestao.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            tabelaQuestao.Dock = DockStyle.Fill;
+            tabelaQuestao.Location = new Point(0, 0);
+            tabelaQuestao.Name = "tabelaQuestao";
+            tabelaQuestao.RowTemplate.Height = 25;
+            tabelaQuestao.Size = new Size(150, 150);
+            tabelaQuestao.TabIndex = 0;
+            // 
+            // TabelaQuestaoControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(tabelaQuestao);
+            Name = "TabelaQuestaoControl";
+            ((System.ComponentModel.ISupportInitialize)tabelaQuestao).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView tabelaQuestao;
+    }
+}
---
+++ GeraTestes.WinApp/ModuloQuestao/TabelaQuestaoControl.cs
@@ -0,0 +1,70 @@
+﻿using GeraTestes.Dominio.ModuloQuestao;
+using GeraTestes.WinApp.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeraTestes.WinApp.ModuloQuestao
+{
+    public partial class TabelaQuestaoControl : UserControl
+    {
+        public TabelaQuestaoControl()
+        {
+            InitializeComponent();
+            tabelaQuestao.ConfigurarGridSomenteLeitura();
+            tabelaQuestao.ConfigurarGridZebrado();
+            tabelaQuestao.Columns.AddRange(ObterColunas());
+        }
+        public DataGridViewColumn[] ObterColunas()
+        {
+            var colunas = new DataGridViewColumn[]
+            {
+                new DataGridViewTextBoxColumn 
+                { 
+                    Name = "Id",
+                    HeaderText = "Id",
+                    FillWeight=15F
+                },
+
+                new DataGridViewTextBoxColumn 
+                { 
+                    Name = "Nome",
+                    HeaderText = "Nome", 
+                    FillWeight=35F 
+                },
+
+                new DataGridViewTextBoxColumn 
+                { 
+                    Name = "Materia.Nome", 
+                    HeaderText = "Matéria",
+                    FillWeight=25F 
+                },
+
+                new DataGridViewTextBoxColumn 
+                { 
+                    Name = "Disciplina.Nome",
+                    HeaderText = "Disciplina",
+                    FillWeight=25F
+                }
+
+            };
+
+            return colunas;
+        }
+        public void AtualizarRegistros(List<Questao> questoes)
+        {
+            tabelaQuestao.Rows.Clear();
+
+            foreach (var questao in questoes)
+            {
+                tabelaQuestao.Rows.Add(questao.Id, questao.Enunciado, questao?.Materia?.Nome, questao.Materia?.Disciplina?.Nome);
+            }
+        }
+    }
+}
