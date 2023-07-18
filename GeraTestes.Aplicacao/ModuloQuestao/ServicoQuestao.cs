2dfd219069e6869a247b206e2fe9277e81047fde
Davi Borges <davifelipe_300@hotmail.com>
2023-07-18 19:14:51 -0300

    inicia o servico questao

---
+++ GeraTestes.Aplicacao/ModuloQuestao/ServicoQuestao.cs
@@ -0,0 +1,22 @@
+﻿using GeraTestes.Dominio.ModuloQuestao;
+
+namespace GeraTestes.Aplicacao.ModuloQuestao
+{
+    public class ServicoQuestao
+    {
+        private IRepositorioQuestao repositorioQuestao;
+
+        private ValidadorQuestao validadorQuestao;
+        private ValidadorAlternativa validadorAlternativa;
+
+        public ServicoQuestao(
+            IRepositorioQuestao _repositorioQuestao,
+            ValidadorQuestao _validadorQuestao, 
+            ValidadorAlternativa _validadorAlternativa)
+        {
+            this.repositorioQuestao = _repositorioQuestao;
+            this.validadorQuestao = _validadorQuestao;
+            this.validadorAlternativa = _validadorAlternativa;
+        }
+    }
+}
