2294199de8ffeb2fd3aa09d04731580617ddd598
Davi Borges <davifelipe_300@hotmail.com>
2023-07-18 19:19:55 -0300

    inicia validador para questoes

---
+++ GeraTestes.Dominio/ModuloQuestao/ValidadorQuestao.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using GeraTestes.Dominio.ModuloMateria;
+
+namespace GeraTestes.Dominio.ModuloQuestao
+{
+    public class ValidadorQuestao : AbstractValidator<Questao>
+    {
+        public ValidadorQuestao()
+        {
+            RuleFor(x => x.Enunciado)
+            .NotEmpty()
+            .NotNull()
+            .MinimumLength(3);
+        }
+    }
+}
