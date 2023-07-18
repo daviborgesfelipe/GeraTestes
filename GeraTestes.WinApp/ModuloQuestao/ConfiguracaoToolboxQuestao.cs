6fcde36e103343e86dc95ca73a01c7c6145b4afe
Davi Borges <davifelipe_300@hotmail.com>
2023-07-18 19:22:37 -0300

    inicia classe para configurar os tooltips da classe questao

---
+++ GeraTestes.WinApp/ModuloQuestao/ConfiguracaoToolboxQuestao.cs
@@ -0,0 +1,12 @@
+﻿using GeraTestes.WinApp.Compartilhado;
+
+namespace GeraTestes.WinApp.ModuloQuestao
+{
+    public class ConfiguracaoToolboxQuestao : ConfiguracaoToolboxBase
+    {
+        public override string TipoCadastro => "Cadastro de Questões";
+        public override string TooltipInserir => "Inserir nova Questão";
+        public override string TooltipEditar => "Editar uma Questão existente";
+        public override string TooltipExcluir => "Excluir uma Questão existente";
+    }
+}
