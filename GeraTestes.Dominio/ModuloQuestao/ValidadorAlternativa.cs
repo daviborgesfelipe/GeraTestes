b97f1ef709098b05df1a95b002fee4ec3c28f0a3
Davi Borges <davifelipe_300@hotmail.com>
2023-07-18 19:19:31 -0300

    inicia o validador para as alternativas

---
+++ GeraTestes.Dominio/ModuloQuestao/ValidadorAlternativa.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace GeraTestes.Dominio.ModuloQuestao
+{
+    public class ValidadorAlternativa : AbstractValidator<Alternativa>
+    {
+        public ValidadorAlternativa()
+        {
+            RuleFor(x => x.Resposta)
+                .NotEmpty()
+                .NotNull()
+                .MinimumLength(3);
+        }
+    }
+}
