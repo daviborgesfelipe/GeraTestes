b3722274f5e88a0850a285fd14034f8bc709b459
Davi Borges <davifelipe_300@hotmail.com>
2023-07-19 13:14:05 -0300

    implementa metodo excluir no servico de disciplina

--- GeraTestes.Aplicacao/ModuloDisciplina/ServicoDisciplina.cs
+++ GeraTestes.Aplicacao/ModuloDisciplina/ServicoDisciplina.cs
@@ -1,4 +1,6 @@
-﻿using GeraTestes.Dominio.ModuloDisciplina;
+﻿using FluentResults;
+using GeraTestes.Dominio.ModuloDisciplina;
+using Microsoft.Data.SqlClient;
 using Serilog;
 using System.Runtime.CompilerServices;
 
@@ -16,6 +18,37 @@ namespace GeraTestes.Aplicacao.ModuloDisciplina
         {
             this.repositorioDisciplina = repositorioDisciplina;
             this.validadorDisciplina = validadorDisciplina;
+        }
+        public Result Excluir(Disciplina disciplina)
+        {
+            Log.Debug("Tentando excluir disciplina...{@d}", disciplina);
+
+            try
+            {
+                repositorioDisciplina.Excluir(disciplina);
+                Log.Debug("Disciplina {DisciplinaId} excluída com sucesso", disciplina.Id);
+
+                return Result.Ok();
+            }
+            catch (SqlException ex)
+            {
+                List<string> erros = new List<string>();
+                string msgErro = ObterMensagemDeErro(ex);
+                erros.Add(msgErro);
+                Log.Error(ex, msgErro + " {DisciplinaId}", disciplina.Id);
+
+                return Result.Fail(erros);
+            }
+        }
+        private static string ObterMensagemDeErro(SqlException ex)
+        {
+            string msgErro;
+            if (ex.Message.Contains("FK_TBMateria_TBDisciplina"))
+                msgErro = "Esta disciplina está relacionada com uma matéria e não pode ser excluída";
+            else
+                msgErro = "Esta disciplina não pode ser excluída";
+
+            return msgErro;
         }
         private List<string> ValidarDisciplina(Disciplina disciplina)
         {
