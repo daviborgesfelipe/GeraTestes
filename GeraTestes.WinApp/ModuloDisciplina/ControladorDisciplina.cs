e86c9ea2e39f89fe09d9965b638ab3d62dbb6fe4
Davi Borges <davifelipe_300@hotmail.com>
2023-07-19 11:27:10 -0300

    altera injecao de dependencia no ctor agr passa instancia do servico de disciplina, implementa metodo inserir, renomeia prop instancia tela principal

--- GeraTestes.WinApp/ModuloDisciplina/ControladorDisciplina.cs
+++ GeraTestes.WinApp/ModuloDisciplina/ControladorDisciplina.cs
@@ -1,4 +1,5 @@
-﻿using GeraTestes.Dominio.ModuloDisciplina;
+﻿using GeraTestes.Aplicacao.ModuloDisciplina;
+using GeraTestes.Dominio.ModuloDisciplina;
 using GeraTestes.WinApp.Compartilhado;
 using System.Runtime.CompilerServices;
 
@@ -8,9 +9,13 @@ namespace GeraTestes.WinApp.ModuloDisciplina
     {
         private TabelaDisciplinaControl tabelaDisciplina;
         private IRepositorioDisciplina repositorioDisciplina;
-        public ControladorDisciplina(IRepositorioDisciplina _repositorioDisciplina)
+        private ServicoDisciplina servicoDisciplina;
+        public ControladorDisciplina(
+            IRepositorioDisciplina _repositorioDisciplina,
+            ServicoDisciplina _servicoDisciplina)
         {
             this.repositorioDisciplina = _repositorioDisciplina;
+            this.servicoDisciplina = _servicoDisciplina;
         }
 
         public override void Excluir()
@@ -20,7 +25,16 @@ namespace GeraTestes.WinApp.ModuloDisciplina
 
         public override void Inserir()
         {
-            throw new NotImplementedException();
+            TelaCadastroDisciplinaForm telaCadastroDisciplina = new TelaCadastroDisciplinaForm();
+            telaCadastroDisciplina.onGravarRegistro += servicoDisciplina.Inserir;
+            telaCadastroDisciplina.ConfigurarDisciplina(new Disciplina());
+
+            DialogResult resultado = telaCadastroDisciplina.ShowDialog();
+
+            if (resultado == DialogResult.OK)
+            {
+                CarregarDisciplinas();
+            }
         }
 
         public override ConfiguracaoToolboxBase ObtemConfiguracaoToolbox()
@@ -45,7 +59,7 @@ namespace GeraTestes.WinApp.ModuloDisciplina
             tabelaDisciplina.AtualizarRegistros(disciplinas);
             mensagemRodape = string.Format("Visualizando {0} disciplina{1}", disciplinas.Count, disciplinas.Count == 1 ? "" : "s");
 
-            TelaPrincipalForm.Instancia.AtualizarRodape(mensagemRodape);
+            TelaPrincipalForm.InstanciaTelaPrincipal.AtualizarRodape(mensagemRodape);
         }
     }
 }
