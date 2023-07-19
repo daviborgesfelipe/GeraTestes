42d81feefa7a71ca5d30dbc97c81a78165625237
Davi Borges <davifelipe_300@hotmail.com>
2023-07-19 13:14:30 -0300

    implementa metodo excluir no repositorio base

--- GeraTestes.Infra/Compartilhado/RepositorioEmSqlBase.cs
+++ GeraTestes.Infra/Compartilhado/RepositorioEmSqlBase.cs
@@ -1,10 +1,94 @@
-﻿namespace GeraTestes.Infra.Sql.Compartilhado
+﻿using GeraTestes.Dominio;
+
+namespace GeraTestes.Infra.Sql.Compartilhado
 {
-    public abstract class RepositorioEmSqlBase
+    public abstract class RepositorioEmSqlBase<TEntidade, TMapeador>
+        where TEntidade : EntidadeBase<TEntidade>
+        where TMapeador : MapeadorBase<TEntidade>, new()
     {
         protected static string enderecoBanco =
             @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=GeradorTesteDb;Integrated Security=True";
 
+        protected abstract string sqlExcluir { get; }
         protected abstract string sqlSelecionarTodos { get; }
+        protected abstract string sqlSelecionarPorId { get; }
+
+        public virtual void Excluir(TEntidade registroSelecionado)
+        {
+            //obter a conexão com o banco e abrir ela
+            SqlConnection conexaoComBanco = new SqlConnection(enderecoBanco);
+            conexaoComBanco.Open();
+
+            //cria um comando e relaciona com a conexão aberta
+            SqlCommand comandoExcluir = conexaoComBanco.CreateCommand();
+            comandoExcluir.CommandText = sqlExcluir;
+
+            //adiciona os parâmetros no comando
+            comandoExcluir.Parameters.AddWithValue("ID", registroSelecionado.Id);
+
+            //executa o comando
+            comandoExcluir.ExecuteNonQuery();
+
+            //encerra a conexão
+            conexaoComBanco.Close();
+        }
+        public virtual TEntidade SelecionarPorId(int id)
+        {
+            //obter a conexão com o banco e abrir ela
+            SqlConnection conexaoComBanco = new SqlConnection(enderecoBanco);
+            conexaoComBanco.Open();
+
+            //cria um comando e relaciona com a conexão aberta
+            SqlCommand comandoSelecionarPorId = conexaoComBanco.CreateCommand();
+            comandoSelecionarPorId.CommandText = sqlSelecionarPorId;
+
+            //adicionar parametro
+            comandoSelecionarPorId.Parameters.AddWithValue("ID", id);
+
+            //executa o comando
+            SqlDataReader leitorItems = comandoSelecionarPorId.ExecuteReader();
+
+            TEntidade registro = null;
+
+            TMapeador mapeador = new TMapeador();
+
+            if (leitorItems.Read())
+                registro = mapeador.ConverterRegistro(leitorItems);
+
+            //encerra a conexão
+            conexaoComBanco.Close();
+
+            return registro;
+        }
+        public virtual List<TEntidade> SelecionarTodos()
+        {
+            //obter a conexão com o banco e abrir ela
+            SqlConnection conexaoComBanco = new SqlConnection(enderecoBanco);
+            conexaoComBanco.Open();
+
+            //cria um comando e relaciona com a conexão aberta
+            SqlCommand comandoSelecionarTodos = conexaoComBanco.CreateCommand();
+            comandoSelecionarTodos.CommandText = sqlSelecionarTodos;
+
+            //executa o comando
+            SqlDataReader leitorItens = comandoSelecionarTodos.ExecuteReader();
+
+            List<TEntidade> registros = new List<TEntidade>();
+
+            TMapeador mapeador = new TMapeador();
+
+            while (leitorItens.Read())
+            {
+                TEntidade registro = mapeador.ConverterRegistro(leitorItens);
+
+                if (registro != null)
+                    registros.Add(registro);
+            }
+
+            //encerra a conexão
+            conexaoComBanco.Close();
+
+            return registros;
+        }
     }
 }
