e3ac132f518790e740299b6a26b820ab4f2bfea2
Davi Borges <davifelipe_300@hotmail.com>
2023-07-19 12:27:57 -0300

    implementa metodos para obter e configurar a disciplina, e click para gravar

--- GeraTestes.WinApp/ModuloDisciplina/TelaCadastroDisciplinaForm.cs
+++ GeraTestes.WinApp/ModuloDisciplina/TelaCadastroDisciplinaForm.cs
@@ -11,10 +11,36 @@ namespace GeraTestes.WinApp.ModuloDisciplina
             InitializeComponent();
             this.ConfigurarDialog();
         }
+        private Disciplina disciplina;
+        public Disciplina ObterDisciplina()
+        {
+            disciplina.Id = Convert.ToInt32(txtId.Text);
+            disciplina.Nome = txtNome.Text;
 
+            return disciplina;
+        }
         private void btnGravar_Click(object sender, EventArgs e)
         {
+            this.disciplina = ObterDisciplina();
 
+            Result resultado = onGravarRegistro(disciplina);
+
+            if (resultado.IsFailed)
+            {
+                string erro = resultado.Errors[0].Message;
+
+                TelaPrincipalForm.Instancia.AtualizarRodape(erro);
+
+                DialogResult = DialogResult.None;
+            }
+        }
+
+        public void ConfigurarDisciplina(Disciplina disciplina)
+        {
+            this.disciplina = disciplina;
+
+            txtId.Text = disciplina.Id.ToString();
+            txtNome.Text = disciplina.Nome;
         }
     }
 }
