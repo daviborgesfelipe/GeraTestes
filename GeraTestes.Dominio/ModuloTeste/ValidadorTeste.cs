3fd69eed175b9d4096b07b1c7eba2866f0fd1e1a
Davi Borges <davifelipe_300@hotmail.com>
2023-07-18 21:41:45 -0300

    inicia validador de teste

---
+++ GeraTestes.Dominio/ModuloTeste/ValidadorTeste.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace GeraTestes.Dominio.ModuloTeste
+{
+    public class ValidadorTeste : AbstractValidator<Teste>
+    {
+        public ValidadorTeste()
+        {
+            RuleFor(x => x.Titulo)
+                .NotEmpty()
+                .NotNull()
+                .MinimumLength(3);
+        }
+    }
+}
