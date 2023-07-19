c362b6292c65e3f0491785e3851ab28fd9fa4ebd
Davi Borges <davifelipe_300@hotmail.com>
2023-07-19 11:29:28 -0300

    inicia o log da aplicacao com serilog e seq

--- GeraTestes.WinApp/Program.cs
+++ GeraTestes.WinApp/Program.cs
@@ -1,3 +1,7 @@
+using FluentValidation;
+using Serilog;
+using System.Globalization;
+
 namespace GeraTestes.WinApp
 {
     internal static class Program
@@ -10,6 +14,19 @@ namespace GeraTestes.WinApp
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+            ValidatorOptions.Global.LanguageManager.Culture = new CultureInfo("pt-BR");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Seq("http://localhost:5341")
+                .CreateLogger();
+
+            /*.WriteTo.File(@"C:\temp\logs\gerador-testes-.txt", 
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message}{NewLine}{Exception}", 
+                    rollingInterval: RollingInterval.Day, retainedFileCountLimit: 5)  */
+
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             ApplicationConfiguration.Initialize();
             Application.Run(new TelaPrincipalForm());
         }
