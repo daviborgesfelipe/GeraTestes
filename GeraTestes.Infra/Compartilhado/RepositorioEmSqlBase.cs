e21847cc598b76019bf9a2a358ec25073b376b9f
Davi Borges <davifelipe_300@hotmail.com>
2023-07-19 12:25:10 -0300

    implementa metodo editar no repositorio base

--- GeraTestes.Infra/Compartilhado/RepositorioEmSqlBase.cs
+++ GeraTestes.Infra/Compartilhado/RepositorioEmSqlBase.cs
@@ -1,10 +1,98 @@
-﻿namespace GeraTestes.Infra.Sql.Compartilhado
+﻿using GeraTestes.Dominio;
+using GeraTestes.Dominio.ModuloDisciplina;
+
+namespace GeraTestes.Infra.Sql.Compartilhado
 {
-    public abstract class RepositorioEmSqlBase
+    public abstract class RepositorioEmSqlBase<TEntidade, TMapeador>
+        where TEntidade : EntidadeBase<TEntidade>
+        where TMapeador : MapeadorBase<TEntidade>, new()
     {
         protected static string enderecoBanco =
             @"Data Source=(localdb)\mssqllocaldb;Initial Catalog=GeradorTesteDb;Integrated Security=True";
 
         protected abstract string sqlSelecionarTodos { get; }
+        protected abstract string sqlEditar { get; }
+        protected abstract string sqlSelecionarPorId { get; }
+
+
+        public virtual void Editar(TEntidade registro)
+        {
+            //obter a conexão com o banco e abrir ela
+            SqlConnection conexaoComBanco = new SqlConnection(enderecoBanco);
+            conexaoComBanco.Open();
+
+            //cria um comando e relaciona com a conexão aberta
+            SqlCommand comandoEditar = conexaoComBanco.CreateCommand();
+            comandoEditar.CommandText = sqlEditar;
+
+            TMapeador mapeador = new TMapeador();
+            //adiciona os parâmetros no comando
+            mapeador.ConfigurarParametros(comandoEditar, registro);
+
+            //executa o comando
+            comandoEditar.ExecuteNonQuery();
+
+            //encerra a conexão
+            conexaoComBanco.Close();
+        }
+
+        public virtual TEntidade SelecionarRegistroPorParametro(string sqlSelecionarPorParametro, SqlParameter[] parametros)
+        {
+            //obter a conexão com o banco e abrir ela
+            SqlConnection conexaoComBanco = new SqlConnection(enderecoBanco);
+            conexaoComBanco.Open();
+
+            //cria um comando e relaciona com a conexão aberta
+            SqlCommand comandoSelecionarPorParametro = conexaoComBanco.CreateCommand();
+            comandoSelecionarPorParametro.CommandText = sqlSelecionarPorParametro;
+
+            foreach (SqlParameter parametro in parametros)
+            {
+                comandoSelecionarPorParametro.Parameters.Add(parametro);
+            }
+
+            //executa o comando
+            SqlDataReader leitorItens = comandoSelecionarPorParametro.ExecuteReader();
+
+            TMapeador mapeador = new TMapeador();
+
+            TEntidade registro = default(TEntidade);
+            if (leitorItens.Read())
+                registro = mapeador.ConverterRegistro(leitorItens);
+
+            //encerra a conexão
+            conexaoComBanco.Close();
+
+            return registro;
+        }
+        public virtual TEntidade SelecionarPorId(int id)
+        {
+            //obter a conexão com o banco e abrir ela
+            SqlConnection conexaoComBanco = new SqlConnection(enderecoBanco);
+            conexaoComBanco.Open();
+
+            //cria um comando e relaciona com a conexão aberta
+            SqlCommand comandoSelecionarPorId = conexaoComBanco.CreateCommand();
+            comandoSelecionarPorId.CommandText = sqlSelecionarPorId;
+
+            //adicionar parametro
+            comandoSelecionarPorId.Parameters.AddWithValue("ID", id);
+
+            //executa o comando
+            SqlDataReader leitorItems = comandoSelecionarPorId.ExecuteReader();
+
+            TEntidade registro = null;
+
+            TMapeador mapeador = new TMapeador();
+
+            if (leitorItems.Read())
+                registro = mapeador.ConverterRegistro(leitorItems);
+
+            //encerra a conexão
+            conexaoComBanco.Close();
+
+            return registro;
+        }
+
     }
 }
