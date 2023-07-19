17d7cf9c7769241246cfae81ba170a6bad6d5669
Davi Borges <davifelipe_300@hotmail.com>
2023-07-18 21:36:05 -0300

    inicia o modulo de servico do teste

---
+++ GeraTestes.Aplicacao/ModuloTeste/ServicoTeste.cs
@@ -0,0 +1,19 @@
+﻿using GeraTestes.Dominio.ModuloTeste;
+
+namespace GeraTestes.Aplicacao.ModuloTeste
+{
+    public class ServicoTeste
+    {
+        private IRepositorioTeste repositorioTeste;
+
+        private ValidadorTeste validadorTeste;
+
+        public ServicoTeste(
+            IRepositorioTeste repositorioTeste,
+            ValidadorTeste validadorTeste)
+        {
+            this.repositorioTeste = repositorioTeste;
+            this.validadorTeste = validadorTeste;
+        }
+    }
+}
