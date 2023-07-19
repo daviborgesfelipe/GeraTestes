7082125550057bbac8fa6ff3a847cce39a0df00d
Davi Borges <davifelipe_300@hotmail.com>
2023-07-19 17:01:56 -0300

    implementa o metodo de obter materia, configurar a tela com as prop da materia, carregar o combo box com as enum da serie, carregar os combo box de disciplina, e click do btn gravar

--- GeraTestes.WinApp/ModuloMateria/TelaCadastroMateriaForm.cs
+++ GeraTestes.WinApp/ModuloMateria/TelaCadastroMateriaForm.cs
@@ -1,20 +1,87 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using GeraTestes.Dominio.ModuloDisciplina;
+using GeraTestes.Dominio.ModuloMateria;
+using GeraTestes.WinApp.Compartilhado;
+using GeraTestes.Dominio.Compartilhado;
+using System.Collections;
 
 namespace GeraTestes.WinApp.ModuloMateria
 {
     public partial class TelaCadastroMateriaForm : Form
     {
-        public TelaCadastroMateriaForm()
+        private Materia materia;
+
+        public event GravarRegistroDelegate<Materia> onGravarRegistro;
+
+        public TelaCadastroMateriaForm(List<Disciplina> disciplinas)
         {
             InitializeComponent();
+            this.ConfigurarDialog();
+            CarregarDisciplinas(disciplinas);
+            CarregarSeries();
+        }
+
+        public Materia ObterMateria()
+        {
+            materia.Id = Convert.ToInt32(txtId.Text);
+            materia.Nome = txtNome.Text;
+            materia.Serie = (SerieMateriaEnum)cmbSeries.SelectedValue;
+            materia.Disciplina = (Disciplina)cmbDisciplinas.SelectedItem;
+
+            return materia;
+        }
+
+        public void ConfigurarMateria(Materia materia)
+        {
+            this.materia = materia;
+
+            txtId.Text = materia.Id.ToString();
+            txtNome.Text = materia.Nome;
+            cmbDisciplinas.SelectedItem = materia.Disciplina;
+            cmbSeries.SelectedValue = materia.Serie;
+        }
+
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            this.materia = ObterMateria();
+
+            Result resultado = onGravarRegistro(materia);
+
+            if (resultado.IsFailed)
+            {
+                string erro = resultado.Errors[0].Message;
+
+                TelaPrincipalForm.Instancia.AtualizarRodape(erro);
+
+                DialogResult = DialogResult.None;
+            }
+        }
+
+        private void CarregarSeries()
+        {
+            SerieMateriaEnum[] series = Enum.GetValues<SerieMateriaEnum>();
+
+            ArrayList items = new ArrayList();
+
+            foreach (Enum serie in series)
+            {
+                var item = KeyValuePair.Create(serie, serie.GetDescription());
+                items.Add(item);
+            }
+
+            cmbSeries.DataSource = items;
+            cmbSeries.DisplayMember = "Value";
+            cmbSeries.ValueMember = "Key";
+        }
+
+        private void CarregarDisciplinas(List<Disciplina> disciplinas)
+        {
+            cmbDisciplinas.Items.Clear();
+
+            foreach (var item in disciplinas)
+            {
+                cmbDisciplinas.Items.Add(item);
+            }
+            cmbDisciplinas.DisplayMember = "Nome";
         }
     }
 }
