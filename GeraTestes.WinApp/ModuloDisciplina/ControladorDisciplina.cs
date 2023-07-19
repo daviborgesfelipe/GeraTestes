934f26e1e05020eab4cb9372e747c19ba5002fcd
Davi Borges <davifelipe_300@hotmail.com>
2023-07-19 13:15:49 -0300

    implementa metodo excluir no controlador de disciplina

--- GeraTestes.WinApp/ModuloDisciplina/ControladorDisciplina.cs
+++ GeraTestes.WinApp/ModuloDisciplina/ControladorDisciplina.cs
@@ -1,4 +1,5 @@
-﻿using GeraTestes.Dominio.ModuloDisciplina;
+﻿using GeraTestes.Aplicacao.ModuloDisciplina;
+using GeraTestes.Dominio.ModuloDisciplina;
 using GeraTestes.WinApp.Compartilhado;
 using System.Runtime.CompilerServices;
 
@@ -8,14 +9,45 @@ namespace GeraTestes.WinApp.ModuloDisciplina
     {
         private TabelaDisciplinaControl tabelaDisciplina;
         private IRepositorioDisciplina repositorioDisciplina;
-        public ControladorDisciplina(IRepositorioDisciplina _repositorioDisciplina)
+        private ServicoDisciplina servicoDisciplina;
+        public ControladorDisciplina(
+            IRepositorioDisciplina _repositorioDisciplina, 
+            ServicoDisciplina servicoDisciplina)
         {
             this.repositorioDisciplina = _repositorioDisciplina;
+            this.servicoDisciplina= servicoDisciplina;
         }
 
         public override void Excluir()
         {
-            throw new NotImplementedException();
+            int id = tabelaDisciplina.ObtemIdSelecionado();
+
+            Disciplina disciplinaSelecionada = repositorioDisciplina.SelecionarPorId(id);
+
+            if (disciplinaSelecionada == null)
+            {
+                MessageBox.Show("Selecione uma disciplina primeiro",
+                "Exclusão de Disciplinas", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            DialogResult opcaoEscolhida = MessageBox.Show("Deseja realmente excluir a disciplina?",
+               "Exclusão de Disciplinas", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+            if (opcaoEscolhida == DialogResult.OK)
+            {
+                Result resultado = servicoDisciplina.Excluir(disciplinaSelecionada);
+
+                if (resultado.IsFailed)
+                {
+                    MessageBox.Show(resultado.Errors[0].Message, "Exclusão de Disciplinas",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    return;
+                }
+
+                CarregarDisciplinas();
+            }
         }
 
         public override void Inserir()
