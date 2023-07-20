5bca6a7f9be65e8e30df47aa08e21cf4df26d15b
Davi Borges <davifelipe_300@hotmail.com>
2023-07-19 23:44:32 -0300

    implementa o metodo selecionar todos

--- GeraTestes.Dominio/ModuloDisciplina/IRepositorioDisciplina.cs
+++ GeraTestes.Dominio/ModuloDisciplina/IRepositorioDisciplina.cs
@@ -5,9 +5,9 @@ namespace GeraTestes.Dominio.ModuloDisciplina
     public interface IRepositorioDisciplina : IRepositorio<Disciplina>
     {
         Disciplina SelecionarPorNome(string nome);
-        //List<Disciplina> SelecionarTodos(
-        //    bool incluirMaterias = false, 
-        //    bool incluirQuestoes = false
-        //    );
+        List<Disciplina> SelecionarTodos(
+            bool incluirMaterias = false,
+            bool incluirQuestoes = false
+            );
     }
 }
