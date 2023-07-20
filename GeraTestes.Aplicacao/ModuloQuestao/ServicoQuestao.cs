26665da3b0a566774a58743da1df87b04629f3bb
Davi Borges <davifelipe_300@hotmail.com>
2023-07-19 23:44:06 -0300

    implementa o metodo inserir no servico de questao

--- GeraTestes.Aplicacao/ModuloQuestao/ServicoQuestao.cs
+++ GeraTestes.Aplicacao/ModuloQuestao/ServicoQuestao.cs
@@ -1,4 +1,7 @@
-﻿using GeraTestes.Dominio.ModuloQuestao;
+﻿using FluentResults;
+using GeraTestes.Dominio.ModuloQuestao;
+using Microsoft.Data.SqlClient;
+using Serilog;
 
 namespace GeraTestes.Aplicacao.ModuloQuestao
 {
@@ -18,5 +21,39 @@ namespace GeraTestes.Aplicacao.ModuloQuestao
             this.validadorQuestao = _validadorQuestao;
             this.validadorAlternativa = _validadorAlternativa;
         }
+        public Result Inserir(Questao questao)
+        {
+            Log.Debug("Tentando inserir questão...{@q}", questao);
+
+            List<string> erros = ValidarQuestao(questao);
+
+            if (erros.Count() > 0)
+                return Result.Fail(erros);
+
+            try
+            {
+                repositorioQuestao.Inserir(questao);
+
+                Log.Debug("Questão {QuestaoId} inserida com sucesso", questao.Id);
+
+                return Result.Ok();
+            }
+            catch (SqlException exc)
+            {
+                string msgErro = "Falha ao tentar inserir questão.";
+
+                Log.Error(exc, msgErro + "{@q}", questao);
+
+                return Result.Fail(msgErro);
+            }
+        }
+        private List<string> ValidarQuestao(Questao questao)
+        {
+            List<string> erros = validadorQuestao.Validate(questao)
+                .Errors.Select(x => x.ErrorMessage).ToList();
+
+            return erros;
+        }
+
     }
 }
